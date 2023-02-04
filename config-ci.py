40a5e6be63d7f1207968461d65f4965907766dc1
ageht <Vladimir.E.Pavlov@yandex.ru>
2023-02-05 01:30:44 +0700

    transfer files

---
+++ config-ci.py
@@ -0,0 +1,7 @@
+class Config:
+    DEBUG = True
+    SECRET_HERE = '249y823r9v8238r9u'
+    # SQLALCHEMY_DATABASE_URI = "sqlite:///./movies.db"
+    SQLALCHEMY_DATABASE_URI = \
+        "postgresql://$DB_USER:$DB_PASSWORD@pg/$DB_NAME"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
