fa0bfa770dd1494c110ccb29f0483628035520a5
ageht <Vladimir.E.Pavlov@yandex.ru>
2023-02-04 22:24:09 +0700

    config

--- config.py
+++ config.py
@@ -3,5 +3,5 @@ class Config:
     SECRET_HERE = '249y823r9v8238r9u'
     # SQLALCHEMY_DATABASE_URI = "sqlite:///./movies.db"
     SQLALCHEMY_DATABASE_URI = \
-        "postgresql://flask_app:Pv3tDf18@localhost/flask_app"
+        "postgresql://flask_app:Pv3tDf18@pg/flask_app"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
